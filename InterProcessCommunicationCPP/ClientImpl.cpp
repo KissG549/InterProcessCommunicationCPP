b550e77465817239aad83b7602843be6c380a18e
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-12-03 10:29:50 +0100

    Client base implemented with WinSock2

--- InterProcessCommunicationCPP/ClientImpl.cpp
+++ InterProcessCommunicationCPP/ClientImpl.cpp
@@ -1,7 +1,16 @@
 #include "ClientImpl.h"
+#include <iostream>
+#include <thread>
+
+/*
+  Based on
+    https://www.winsocketdotnetworkprogramming.com/winsock2programming/winsock2advancedcode1d.html
+*/
 
 ClientImpl::ClientImpl()
+    :mSocket(),mServerAddr(),mSenderInfo(),mBuffer("")
 {
+    mFutureObj = mExitSignal.get_future();
 }
 
 ClientImpl::~ClientImpl()
@@ -10,6 +19,41 @@ ClientImpl::~ClientImpl()
 
 void ClientImpl::run(std::string& pServerAddr, int pPort)
 {
+    std::cout << "Connecting to the server: "
+        << pServerAddr
+        << ":"
+        << pPort
+        << std::endl;
+
+    while (!connect(pServerAddr, pPort))
+    {
+        /* Wait 1 second before reconnect */
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "Trying to connect to the server: " 
+            << pServerAddr 
+            << ":" 
+            << pPort 
+            << std::endl;
+    }
+
+    /* Receive packets on independent thread */
+    std::thread receiverTh( ClientImpl::receive, this);
+
+    send("Sample message");
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    mExitSignal.set_value();
+
+    std::cout << "Ask receiver thread to stop." << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+
+    std::cout << "Receiver thread timed out." << std::endl;
+
+    receiverTh.detach();
+
+    /* Close the connection */
+    disconnect();
 }
 
 bool ClientImpl::connect(std::string pServerAddr, int pPort)
@@ -21,8 +65,12 @@ void ClientImpl::disconnect()
 {
 }
 
-void ClientImpl::receive()
+void ClientImpl::receive(ClientImpl* pClient)
 {
+    while (pClient->mFutureObj.wait_for(std::chrono::milliseconds(10)) == std::future_status::timeout )
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 }
 
 int ClientImpl::send(std::string pMessage)
--- InterProcessCommunicationCPP/ClientImpl.h
+++ InterProcessCommunicationCPP/ClientImpl.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "IPCAppBase.h"
+#include <future>
 
 #if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
 //
 //#include <windows.h>
-//#include <WinSock2.h>
+#include <WinSock2.h>
 
 #endif // 
 
@@ -19,11 +20,20 @@ public:
 
 private:
 
-    //struct sockaddr_in mServerAddr;
+    /* from WinSock2 */
+    SOCKET mSocket;
+    SOCKADDR_IN mServerAddr;
+    SOCKADDR_IN mSenderInfo;
+    /* END form WinSock2 */
+    std::string mBuffer;
+//    std::atomic_bool mDone;
+
+    std::promise<void> mExitSignal;
+    std::future<void> mFutureObj;
 
     bool connect(std::string pServerAddr, int pPort);
     void disconnect();
-    void receive();
+    static void receive(ClientImpl* pClient);
     int send(std::string pMessage);
 };
 
